dd4f3e730b17cb883efdb618f3c8bc8195b061bb
Nitikesh Bhad <nitikesh.bhad@here.com>
2018-03-19 20:25:25 +0530

    Initial

---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_a1330.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_a1330.dir/feature_tests.c.o"
+  "cmTC_a1330.pdb"
+  "cmTC_a1330"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_a1330.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_a1330.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_a1330.
+# This may be replaced when dependencies are built.
